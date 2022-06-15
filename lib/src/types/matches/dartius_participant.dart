4eb9d28b33c2784b6524dda030d03f752b1ea21e
Ezan777 <enrizangri@gmail.com>
2022-06-15 21:19:07 +0200

    Added right builder to participant

--- lib/src/types/matches/dartius_participant.dart
+++ lib/src/types/matches/dartius_participant.dart
@@ -20,16 +20,38 @@ enum Positions {
 /// Class [Participant] is made to represent players of a single match, it will
 /// contains data of the players like kills, assists, deaths and others...
 class Participant {
-  String _puuid;
-  Positions _position;
-  late Champion _champion;
-  late bool _winner, _firstBloodKill, _pentakill;
-  late String _summonerName;
-  late int _totalDamageToChampion, _totalCs, _visionScore,
-   _summoner1Id, _summoner2Id, _kills, _deaths, _assists, _champLevel, _goldEarned;
+  String _puuid, _summonerName;
+  Champion _champion;
+  bool _winner, _firstBloodKill, _pentakill;
+  int _totalDamageToChampions,
+      _totalCs,
+      _visionScore,
+      _summoner1Id,
+      _summoner2Id,
+      _kills,
+      _deaths,
+      _assists,
+      _champLevel,
+      _goldEarned;
 
-  Participant(Positions position, String puuid)
-      : _puuid = puuid,
-        _position = position;
-  
+  Participant(Map<String, dynamic> participantJson)
+      : _puuid = participantJson['puuid'],
+        _champion = Champion(
+            participantJson['championId'], participantJson['championName']),
+        _winner = participantJson['winner'],
+        _firstBloodKill = participantJson['firstBloodKill'],
+        _pentakill = participantJson['pentaKills'] >= 1 ? true : false,
+        _summonerName = participantJson['summonerName'],
+        _totalDamageToChampions =
+            participantJson['totalDamageDealtToChampions'],
+        _totalCs = participantJson['totalMinionsKilled'] +
+            participantJson['neutralMinionsKilled'],
+        _visionScore = participantJson['visionScore'],
+        _summoner1Id = participantJson['summoner1Id'],
+        _summoner2Id = participantJson['summoner2Id'],
+        _kills = participantJson['kills'],
+        _deaths = participantJson['deaths'],
+        _assists = participantJson['assists'],
+        _champLevel = participantJson['champLevel'],
+        _goldEarned = participantJson['goldEarned'];
 }
