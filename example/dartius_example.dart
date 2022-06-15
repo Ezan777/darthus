d2066df3aa223eb394a18f655845762f80b42a9b
Ezan777 <enrizangri@gmail.com>
2022-06-15 22:13:09 +0200

    Added a second summoner to the example

--- example/dartius_example.dart
+++ example/dartius_example.dart
@@ -1,15 +1,18 @@
 import 'package:dartius/dartius.dart';
 
 void main() async {
-  final String summonerName = 'zan777';
+  final String summonerName = 'zan777', summoner2Name = 'bondighidighi';
   // Check if the summoner name is valid, in order to avoid connection errors
   if (await Summoner.summonerNameIsValid(summonerName)) {
     // Create the summoner object that at the moment it's empty
     Summoner summoner = Summoner('euw1', summonerName);
+    Summoner summoner2 = Summoner('euw1', summoner2Name);
     // Retrieve all data about the summoner from Riot API
     await summoner.buildSummoner(); // Now summoner is ready for use
+    await summoner2.buildSummoner();
 
     print('$summonerName is ${summoner.rankSolo()}');
+    print('$summoner2Name is ${summoner2.rankSolo()}');
   } else {
     print('Invalid summoner name!');
   }
