ecd90a7c649e5bf197fa49f52eed8d03cb0188fb
Ezan777 <enrizangri@gmail.com>
2022-06-16 11:18:44 +0200

    Finished match constructor and added function to retrieve match duration

--- lib/src/types/matches/dartius_match.dart
+++ lib/src/types/matches/dartius_match.dart
@@ -1,16 +1,59 @@
+import 'dart:math';
+
 import 'package:dartius/src/dartius_riot_api.dart';
 import 'package:dartius/src/types/matches/dartius_teams.dart';
 
+class MatchNotBuilt implements Exception {}
+
 class Match {
-  late List<Team> teams;
-  late int _gameDuration;
+  late List<Team>? _teams;
+  late int? _gameDuration;
   final String _matchId, _region;
 
-  Match(String region, String matchId) : _matchId = matchId, _region = region;
+  Match(
+      {required String region,
+      required String matchId,
+      int? gameDuration,
+      List<Team>? teams})
+      : _matchId = matchId,
+        _region = region,
+        _gameDuration = gameDuration,
+        _teams = teams;
+
+  factory Match.fromJson(Map<String, dynamic> matchJson, String region) {
+    final matchId = matchJson['metadata']['matchId'];
+    final gameDuration = matchJson['info']['gameDuration'];
+    List<Team> teams = [
+      Team((matchJson['info']['participants'] as List).sublist(0, 4)),
+      Team((matchJson['info']['participants'] as List).sublist(5, 9))
+    ];
+
+    return Match(
+        region: region,
+        matchId: matchId,
+        gameDuration: gameDuration,
+        teams: teams);
+  }
 
   Future<void> getFromApi() async {
     final matchJson = await allMatchInfo(_region, _matchId);
 
-    _gameDuration = matchJson['info']['gameDuration'];
+    Match.fromJson(matchJson, _region);
+  }
+
+  /// Returns the game duration of the match in minutes with the following format
+  /// : minutes:seconds
+  String gameDurationInMinutes() {
+    if (_gameDuration != null) {
+      final int minutes = _gameDuration! ~/ 60;
+      final int seconds = (minutes - (_gameDuration! / 60) * 60).toInt();
+      return '$minutes:$seconds';
+    } else {
+      throw MatchNotBuilt();
+    }
+  }
+
+  int gameDurationInSeconds() {
+    return _gameDuration ?? (throw MatchNotBuilt);
   }
 }
